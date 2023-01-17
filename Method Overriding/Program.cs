75cb6c4924472b079e3b667af6485f1649795409
Aarif3 <121675091+Aarif3@users.noreply.github.com>
2023-01-17 10:45:26 +0530

    Add files via upload

---
+++ Method Overriding/Program.cs
@@ -0,0 +1,31 @@
+﻿namespace Method_Overriding
+{
+    class parent
+    {
+        public virtual void Show(int id)
+        {
+            Console.WriteLine("Your id is {0}",id);
+        }
+    }
+
+    class child : parent
+    {
+        public override void Show(int age)
+        {
+            Console.WriteLine("Your age is{0}",age);
+        }
+    }
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Enter id or age");
+            int val = int.Parse(Console.ReadLine());
+
+            parent c1 = new parent();
+            c1.Show(val);
+        }
+    }
+}
