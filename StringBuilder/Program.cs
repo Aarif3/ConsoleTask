63deb5147f4dc082c0d661f2750d747902d3467a
Aarif3 <121675091+Aarif3@users.noreply.github.com>
2023-01-17 10:49:22 +0530

    Add files via upload

---
+++ StringBuilder/Program.cs
@@ -0,0 +1,19 @@
+﻿using System.Text;
+using System;
+
+namespace StringBuilders
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            StringBuilder sb = new StringBuilder();
+            sb.AppendLine("Hello");
+            sb.AppendLine("World");
+            sb.Append("hii");
+            Console.WriteLine(sb);
+            sb.AppendJoin("hello","hello");
+            Console.WriteLine(sb);
+        }
+    }
+}
