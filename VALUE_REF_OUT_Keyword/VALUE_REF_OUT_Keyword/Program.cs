8a0362d8e516cd8c7f2df0050c50f999dd81054c
Aarif3 <121675091+Aarif3@users.noreply.github.com>
2023-01-17 10:49:54 +0530

    Add files via upload

---
+++ VALUE_REF_OUT_Keyword/VALUE_REF_OUT_Keyword/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VALUE_REF_OUT_Keyword
+{
+    internal class Program
+    {
+        /*
+        public static void PassByRef(ref int a)
+        {
+            a = a + 5;
+            Console.WriteLine("value is {0}",a);
+        }
+
+
+        static void Main(string[] args)
+        {
+            int value = 5;
+            PassByRef(ref value);
+            Console.WriteLine(value);
+            Console.ReadLine();
+        }
+        */
+
+        public static void PassByOut(out int c)
+        {
+            c = 5;
+            Console.WriteLine(c);
+        }
+
+        static void Main(string[] args)
+        {
+            int value = 10; // this value will change to 5....
+            PassByOut(out value);
+            Console.WriteLine(value);
+            Console.ReadLine();
+        }
+    }
+}
