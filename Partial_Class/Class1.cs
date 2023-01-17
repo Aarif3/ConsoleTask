64f2bf0c61141896040137264d4d0577c151008e
Aarif3 <121675091+Aarif3@users.noreply.github.com>
2023-01-17 10:47:07 +0530

    Add files via upload

---
+++ Partial_Class/Class1.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Partial_Class
+{
+    public partial class Student
+    {
+        static void Main(string[] args)
+        {
+            Student s1 = new Student();
+            s1._id = 10;
+            s1.Name = "abcd";
+            Console.WriteLine(s1._name);
+            Console.WriteLine(s1._id);
+        }
+    }
+}
---
+++ Partial_Class/Class2.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Partial_Class
+{
+    public partial class Student
+    {
+        private int _id;
+        private string _name;
+
+        public int detail
+        {
+            set
+            {
+                _id = value;
+            }
+            get
+            {
+                return _id;
+            }
+        }
+
+        public string Name
+        {
+            set
+            {
+                _name = value;
+            }
+            get
+            {
+                return _name;
+            }
+        }
+
+    }
+}
