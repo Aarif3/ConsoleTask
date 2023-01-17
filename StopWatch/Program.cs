4f51a435007147ad54c0524d52e6f7f826bb0239
Aarif3 <121675091+Aarif3@users.noreply.github.com>
2023-01-17 10:48:42 +0530

    Add files via upload

---
+++ StopWatch/Program.cs
@@ -0,0 +1,23 @@
+﻿using System.Diagnostics;
+namespace StopWatch
+{
+
+
+    internal class Program
+    {
+          
+        static async Task Main(string[] args)
+        {
+            var sw = new Stopwatch();
+            sw.Start();
+
+
+            Console.WriteLine("Hello word");
+            //waiting for 5000 milisecond
+            await Task.Delay(5000);
+            sw.Stop();
+            var timeTaken = sw.ElapsedMilliseconds;
+            Console.WriteLine($"Time Take is {timeTaken}");
+        }
+    }
+}
