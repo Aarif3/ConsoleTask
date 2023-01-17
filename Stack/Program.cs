11f116d688cc7a81954df0ad812481e01acd9169
Aarif3 <121675091+Aarif3@users.noreply.github.com>
2023-01-17 10:47:55 +0530

    Add files via upload

---
+++ Stack/Program.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+
+namespace Stacks
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Stack s1= new Stack();
+            s1.Push("Abc");
+            s1.Push(1);
+            s1.Push(2.22);
+            s1.Push('a');
+            //Item Puched
+            
+            //pop 
+            s1.Pop();
+
+            s1.Peek();
+
+            
+            s1.Contains("abc");
+            
+            s1.Clear();
+            foreach (object i in s1)
+            {
+                Console.WriteLine(i);
+            }
+        }
+    }
+}
