09b29ec9dd6cd5bac922daffc6fa1acd60f315d3
Aarif3 <121675091+Aarif3@users.noreply.github.com>
2023-01-17 10:42:06 +0530

    Add files via upload

---
+++ Encapsulation/Program.cs
@@ -0,0 +1,46 @@
+﻿namespace Encapsulation
+{
+    public class student
+    {
+        public int id;
+        public string name;
+
+        public void setName(string name,int id)
+        {
+            if (string.IsNullOrEmpty(name))
+            {
+                Console.WriteLine("Name is required");
+            }
+            else
+            {
+
+
+                this.name = name;
+                this.id = id;
+            }
+        }
+
+        public void getName()
+        {
+            if(this.name == null)
+            {
+
+            }
+            else {
+                Console.WriteLine("Id is ", +this.id);
+                Console.WriteLine("Name is {0}", this.name);
+
+            }
+        }
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            student s1 = new student();
+            s1.setName("abcd",123);
+            
+
+        }
+    }
+}
