7fcd30e023f611003cb988ab06b9d171fe8c2fa7
Aarif3 <121675091+Aarif3@users.noreply.github.com>
2023-01-17 10:46:20 +0530

    Add files via upload

---
+++ Method_Overloading/Program.cs
@@ -0,0 +1,41 @@
+﻿namespace Method_Overloading
+{
+    internal class Program
+    {
+        //Method
+        public static void Student()
+        {
+            Console.WriteLine("Default one");
+        }
+        //Method Overloading
+        public static void Student(int id, string name)
+        {
+
+            Console.WriteLine("Your id is {0} ", id);
+            Console.WriteLine("Your Name is {0} ",name);
+        }
+        //method Overloading
+        public static void Student(int id, string name, int age)
+        {
+            Console.WriteLine("Your id is {0}",id);
+            Console.WriteLine("Your name is {0}", name);
+            Console.WriteLine("Your age is {0}", age);
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter your id");
+            int id =int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Enter Your Name");
+            string name =Console.ReadLine();
+
+            Console.WriteLine("Enter your age");
+            int age =int.Parse(Console.ReadLine());
+
+            //Directly call by class name because i makes Methods statics
+            Program.Student();
+            Program.Student(id,name);
+            Program.Student(id,name, age);
+        }
+    }
+}
